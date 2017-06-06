b1cd1c672369d341ad323fcd319cd6b082b4c11c
amalakamala <amalakamala.a@gmail.com>
2017-06-06 12:10:30 -0400

    botonera response

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+var menu = document.getElementById("menu");
+
+menu.addEventListener("click",function(){
+	var elnav = document.getElementById("nav");
+	elnav.classList.toggle("menu-nav");
+
+
+})
+
